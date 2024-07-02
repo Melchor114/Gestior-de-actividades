f5459bea49fb00db91ea7605bb8db775a00c3d74
LUIS ANTONIO RAGA REYES <2130129@upv.edu.mx>
2024-07-02 08:46:57 -0600

    inicio, registro, email

---
+++ app/src/main/java/com/example/project/Registro.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registrar), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void openInicio(View view) {
+        Intent intent = new Intent(Registro.this, inicio.class);
+        startActivity(intent);
+        // Finalizar la actividad actual si es necesario
+        finish();
+    }
+}
