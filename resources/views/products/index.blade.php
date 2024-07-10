290d59b4bfe9f3ef077d9332659459ca798b3335
LUIS ANTONIO RAGA REYES <2130129@upv.edu.mx>
2024-07-09 23:11:17 -0600

    diseño de login

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,169 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Listado Productos') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @session('success')
+                        <div class="alert alert-success" role="alert">
+                            {{ $value }}
+                        </div>
+                    @endsession
+
+                    <div class="bg-white shadow rounded-lg overflow-hidden">
+                        <div class="bg-gray-100 px-4 py-2 font-semibold text-gray-700">
+                            Product List
+                        </div>
+                        <div class="p-4">
+                            <a href="{{ route('products.create') }}"
+                                class="inline-flex items-center px-3 py-2 bg-green-500 text-white text-sm font-medium rounded hover:bg-green-600 transition duration-150 my-2">
+                                <i class="bi bi-plus-circle mr-1"></i> Add New Product
+                            </a>
+                            <div class="overflow-x-auto">
+                                <table class="min-w-full bg-white border border-gray-200 divide-y divide-gray-200">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                S#</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                nombre</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                precio_venta</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                precio_compra</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                fecha_compra</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                        @forelse ($products as $product)
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-nowrap">{{ $loop->iteration }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">{{ $product->nombre }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">{{ $product->precio_venta }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">{{ $product->precio_compra }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">{{ $product->fecha_compra }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <form action="{{ route('products.destroy', $product->id_producto) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <a href="{{ route('products.show', $product->id_producto) }}"
+                                                            class="inline-flex items-center px-2 py-1 bg-yellow-500 text-white text-xs font-medium rounded hover:bg-yellow-600 transition duration-150">
+                                                            <i class="bi bi-eye mr-1"></i> Show
+                                                        </a>
+                                                        <a href="{{ route('products.edit', $product->id_producto) }}"
+                                                            class="inline-flex items-center px-2 py-1 bg-blue-500 text-white text-xs font-medium rounded hover:bg-blue-600 transition duration-150">
+                                                            <i class="bi bi-pencil-square mr-1"></i> Edit
+                                                        </a>
+                                                        <button type="submit"
+                                                            class="inline-flex items-center px-2 py-1 bg-red-500 text-white text-xs font-medium rounded hover:bg-red-600 transition duration-150"
+                                                            onclick="return confirm('Do you want to delete this product?');">
+                                                            <i class="bi bi-trash mr-1"></i> Delete
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="6" class="px-6 py-4 whitespace-nowrap text-center">
+                                                    <span class="text-red-500 font-semibold">
+                                                        No Product Found!
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            {{-- <div class="mt-4">
+                                {{ $products->links() }}
+                            </div> --}}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
+
+
+{{-- <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Lista de productos') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-12">
+
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-center">S#</th>
+                            <th scope="col" class="px-6 py-3">Code</th>
+                            <th scope="col" class="px-6 py-3">Name</th>
+                            <th scope="col" class="px-6 py-3">Quantity</th>
+                            <th scope="col" class="px-6 py-3">Price</th>
+                            <th scope="col" class="px-6 py-3">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($products as $product)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="px-6 py-4 text-center">{{ $loop->iteration }}</td>
+                            <td class="px-6 py-4">{{ $product->code }}</td>
+                            <td class="px-6 py-4">{{ $product->name }}</td>
+                            <td class="px-6 py-4">{{ $product->quantity }}</td>
+                            <td class="px-6 py-4">{{ $product->price }}</td>
+                            <td class="px-6 py-4 text-right">
+                                <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <a href="{{ route('products.show', $product->id) }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><i class="bi bi-eye"></i> Show</a>
+
+                                    <a href="{{ route('products.edit', $product->id) }}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><i class="bi bi-pencil-square"></i> Edit</a>
+
+                                    <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onclick="return confirm('Do you want to delete this product?');"><i class="bi bi-trash"></i> Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="6" class="px-6 py-4">
+                                <span class="text-danger">
+                                    <strong>No Product Found!</strong>
+                                </span>
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
+
+            {{ $products->links() }}
+        </div>
+    </div>
+
+</x-app-layout> --}}
